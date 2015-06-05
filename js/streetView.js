8f924a058b5b1ba257709d2b30e877d19b0ade13
Qiao Lin <qiaoclin@gmail.com>
2015-06-05 15:24:13 -0400

    streetView randomize

--- js/streetView.js
+++ js/streetView.js
@@ -3,60 +3,78 @@ var myPano;
 function initialize() {
   //let's go anywhere else in the world 
   getIt(); 
-
 }
 
 function getIt(){
-  var lat = Math.floor(Math.random()*180)-90
-  var lng = Math.floor(Math.random()*360)-180
 
-  var elseWhere = new google.maps.LatLng(lat, lng);
-  //37.869260, -122.254811
-  var panoramaOptions = {
-    position: elseWhere,
-    pov: {
-      heading: 165,
-      pitch: 0
-    },
-    zoom: 1,
-    addressControlOptions: {
-        position: google.maps.ControlPosition.BOTTOM_CENTER
-    },
-    linksControl: false,
-    panControl: false,
-    zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.SMALL
-    },
-    enableCloseButton: false
-  };
+  var lat, lng, elseWhere, panoramaOptions;
+  function setMapDetails() {
+    var latLngRanges = [
+      {
+        // america
+        lat: { mn: 25, mx: 55 },
+        lng: { mn: -125, mx: -70 }
+      },
+      {
+        // europe
+        lat: { mn: 35, mx: 70 },
+        lng: { mn: 10, mx: 30 }
+      }
+    ]
+
+  var range = latLngRanges[Math.floor(Math.random() * latLngRanges.length)]
+    lat = Math.random() * (range.lat.mx - range.lat.mn) + range.lat.mn;
+    lng = Math.random() * (range.lng.mx - range.lng.mn) + range.lng.mn;
+    elseWhere = new google.maps.LatLng(lat, lng);
+
+    panoramaOptions = {
+      position: elseWhere,
+      pov: {
+        heading: 165,
+        pitch: 0
+      },
+      zoom: 1,
+      addressControlOptions: {
+          position: google.maps.ControlPosition.BOTTOM_CENTER
+      },
+      linksControl: false,
+      panControl: false,
+      zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL
+      },
+      enableCloseButton: false
+    };
+  }
+
+  setMapDetails();
 
   myPano = new google.maps.StreetViewPanorama(
-      document.getElementById('map-canvas'),
-      panoramaOptions);
+  document.getElementById('map-canvas'),
+  panoramaOptions);
 
-  console.log(lat);
-  console.log(lng);
-   
-  // console.log(myPano.data.projection);
-  addGMapListeners();
+  checkResults();
+  function checkResults() {
+    if (myPano.getStatus()) {
+      if (myPano.getStatus() === 'ZERO_RESULTS') {
+        // try a different lat/lng
+        setMapDetails();
+        myPano = new google.maps.StreetViewPanorama(
+        document.getElementById('map-canvas'),
+        panoramaOptions);
+        checkResults();
+      } else {
+        // you win, render it
+        console.log(myPano.projection);
+        myPano.setVisible(true);
+      }
+    } else {
+      // keep waiting
+      setTimeout(function(){
+        checkResults();
+      }, 1);
+    }
+  }
+  
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-function addGMapListeners() {
-
-  debugger;
-google.maps.event.addListener(myPano, 'pano_changed', function() {
-      // var panoCell = document.getElementById('map-canvas');
-      console.log(myPano.projection);
-      if (myPano.projection != undefined ){
-        
-        myPano.setVisible(true);
-      }else{
-        debugger;
-        console.log(myPano.projection);
-        getIt();
-      }
-  });
-myPano.setVisible(false);
-}
