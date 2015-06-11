68c376e06008d3ca4f43eeacafca8c9b4519a6a1
Qiao Lin <qiaoclin@gmail.com>
2015-06-11 14:19:37 -0400

    psql

--- server.js
+++ server.js
@@ -12,8 +12,21 @@ var pg = require('pg');
 //var connectStr = "pg://"+secrets["username"]+ ":"+ secrets["password"]+"@localhost/homeslice"; 
 var connectStr = "pg://root@localhost/root"; 
 var client = new pg.Client(connectStr); 
-client.connect(function(){
-  console.log("connected to psql");
+// client.connect(function(){
+//   console.log("connected to psql");
+// });
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
 });
 
 app.use(cors());
